ba10cadcdf01a312b847b61a561ab16832a40319
ezoehunt <ezoehunt@gmail.com>
2017-02-15 03:09:03 -0800

    Organize files

--- config.rb
+++ config.rb
@@ -1,24 +1,21 @@
-###
-# Page options, layouts, aliases and proxies
-###
+activate :directory_indexes
+activate :autoprefixer
+activate :gzip
+activate :sprockets
 
-# Per-page layout changes:
-#
+set :relative_links, true
+
+set :css_dir, "assets/css"
+set :js_dir, "assets/js"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+
 # With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
+# Reload browser whenever files change
 configure :development do
   activate :livereload
 end
@@ -36,9 +33,20 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  activate :relative_assets
+  #activate :minify_css
+  #activate :minify_javascript
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  # ignore during build so assets arent
+  # recreated again in /build
+  ignore "assets/js/vendor/*"
+  ignore "assets/css/vendor/style.scss"
+  ignore "assets/css/vendor/bootstrap/*"
+  ignore "assets/css/vendor/font-awesome/*"
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.branch = 'gh-pages'
+  deploy.deploy_method = :git
 end
